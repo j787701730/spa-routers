446e77c28de36c3e2a0ac5f2e931ed76c90dbb4f
zhengw <247276359@qq.com>
2021-04-19 17:08:33 +0800

    加入jQuery

--- route/js/detail.js
+++ route/js/detail.js
@@ -1,4 +1,22 @@
-SPA_RESOLVE_INIT = function(transition) { 
-	document.getElementById("content").innerHTML = '<p style="color:red;">当前异步渲染详情页'+ JSON.stringify(transition) +'</p>'
-	console.log("首页回调" + JSON.stringify(transition))
-}
+SPA_RESOLVE_INIT = function (transition) {
+  console.log($);
+  function foo() {
+    console.log($(this).html());
+  }
+  $("#content").off("click", "p", "**");
+  document.getElementById("content").innerHTML =
+    '<p class="bg-primary" style="color:red;">当前异步渲染详情页' +
+    JSON.stringify(transition) +
+    '</p><button class="btn">点我</button><input class="form-control"/>';
+  console.log("首页回调" + JSON.stringify(transition));
+  $("#content")
+    .on("click", "p", foo)
+    .on("keydown", ".form-control", function (e) {
+      console.log(e.keyCode);
+    })
+    .on("click", ".btn", function () {
+      alert("btn");
+    });
+
+  $("#content input").val("hello world");
+};
