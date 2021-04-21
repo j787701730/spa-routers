3c090a485adb9232b22530592bc23521cd4357dc
zhengw <247276359@qq.com>
2021-04-21 14:24:28 +0800

    模拟登录页面

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,15 +3,19 @@ didMount = function (props) {
   $content.html(
     '<h3>当前异步渲染首页</h3><p style="color:#099fde;">' +
       JSON.stringify(props) +
-      '</p><button class="btn">点我</button><input class="form-control"/>'
+      '</p><button class="btn msg-btn">点我</button><input class="form-control"/><button class="btn out-btn">退出</button>'
   );
   console.log("首页回调" + JSON.stringify(props));
-  $(".btn").click(function () {
+  $(".msg-btn").click(function () {
     layer.msg("首页回调" + JSON.stringify(props), {
       icon: 1,
       shade: 0.3,
       time: 2000,
     });
+  });
+
+  $(".out-btn").click(function () {
+    window.location.href = "login.html#/login";
   });
 };
 
